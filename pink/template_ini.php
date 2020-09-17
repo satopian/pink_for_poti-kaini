a5cc4b8314a50f875a69b09777af19d2a2e85b1e
satopian <satopian@mia.eek.jp>
2020-09-17 18:52:36 +0900

    lot.200917

--- pink/template_ini.php
+++ pink/template_ini.php
@@ -1,11 +1,11 @@
 <?php
 /*
-  * Template - PINK - lot.200902  by さとぴあ  >> https://pbbs.sakura.ne.jp/
+  * Template - PINK - lot.200917  by さとぴあ  >> https://pbbs.sakura.ne.jp/
   *
 */
 
 //テンプレートのバージョン
-define('TEMPLATE_VER', "lot.200902");
+define('TEMPLATE_VER', "lot.200917");
 
 
 /* ---------- ADD:2004/06/22 ---------- */
